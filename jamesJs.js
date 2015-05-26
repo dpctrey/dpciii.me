6b8fa89c1bba09aa91bccb968a206e68f91fca28
Dwight Patton Christian III <dchristian@jsu.edu>
2015-05-25 19:50:43 -0500

    Create jamesJs.js

---
+++ jamesJs.js
@@ -0,0 +1,4 @@
+var myMap = document.getElementById("map_view");
+myMap.onclick = function() {
+	var myVar = window.open("https://www.google.com/maps/place/Jamestown+Apartments/@33.8189075,-85.7699636,15z/data=!4m2!3m1!1s0x888a4e015b7599ef:0x4538adea1d0f3acf");
+};
